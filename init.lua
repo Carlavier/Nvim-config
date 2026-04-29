9f7bbee5acca9963a6df5e892e97c8e920138859
Carlavier <namto10042004@gmail.com>
2026-04-29 15:23:18 +0700

    [modify] clean up plugins

--- init.lua
+++ init.lua
@@ -1,18 +1,25 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+require('lazy').setup({
   spec = {
-    { import = "plugins" },
+    { import = 'plugins' },
   },
 })
 
-require("config")
-require("autocmd")
-
+require('config')
+require('autocmd')
